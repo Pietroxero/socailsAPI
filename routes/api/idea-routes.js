ba9b31129082f9654073ebe46b56f9b961fe6ebb
Pietroxero <115331791+Pietroxero@users.noreply.github.com>
2023-03-27 19:01:34 -0700

    added in code for idea-route js file

--- routes/api/idea-routes.js
+++ routes/api/idea-routes.js
@@ -12,3 +12,19 @@ const {
     deleteReaction
 } = require ('../../controllers/idea-controller');
 
+// This will be our get route
+router.route ('/').get(getAllIdeas);
+
+//this will be our get, put, deletes
+router.route ('/:id').get(getIdeasById).put(updateIdeas).delete(deleteIdeas);
+
+//this will be the post route
+router.route ('/:userId').post(createIdeas);
+
+//post route for reactions
+router.route ('/:ideasId/reactions').post(addReaction);
+
+// delete route
+router.route ('/:ideasId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
